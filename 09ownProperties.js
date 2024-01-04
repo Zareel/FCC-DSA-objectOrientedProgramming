e2e44d8747a70eb52c220aec10eb211b55e84adc
Zareel Kalam <zareelbwd@gmail.com>
2024-01-04 15:46:43 +0530

    completed more tasks

---
+++ 09ownProperties.js
@@ -0,0 +1,22 @@
+// * Understand Own Properties
+
+//? Add the own properties of canary to the array ownProps.
+
+//create a constructor MySelf,
+//it takes two parameters name and age
+//create two new instances of MySelf constuctor
+
+function Bird(name) {
+  this.name = name;
+  this.numLegs = 2;
+}
+
+let canary = new Bird("Tweety");
+let ownProps = [];
+// Only change code below this line
+
+for (let property in canary) {
+  if (canary.hasOwnProperty(property)) {
+    ownProps.push(property);
+  }
+}
