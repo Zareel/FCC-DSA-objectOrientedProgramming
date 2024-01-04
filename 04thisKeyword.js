4579e210cf7d8f7b5c91f90ce14a938cd67c3027
Zareel Kalam <zareelbwd@gmail.com>
2024-01-04 12:09:03 +0530

    completed more tasks

---
+++ 04thisKeyword.js
@@ -0,0 +1,13 @@
+//* Make Code More Reusable with the this Keyword
+
+//? Modify the dog.sayLegs method to remove any references to dog. Use the duck example for guidance.
+
+let dog = {
+  name: "Spot",
+  numLegs: 4,
+  sayLegs: function () {
+    return "This dog has " + this.numLegs + " legs.";
+  },
+};
+
+console.log(dog.sayLegs());
---
+++ 05constructor.js
@@ -0,0 +1,9 @@
+//* Define a Constructor Function
+
+//? te a constructor, Dog, with properties name, color, and numLegs that are set to a string, a string, and a number, respectively.
+
+function Myself() {
+  (this.name = "Shon"),
+    (this.age = 19),
+    (this.profession = "MERN Stack developer");
+}
