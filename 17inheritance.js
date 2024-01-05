4f03a705f8539bb8f2ce8e2a0362a73f5ef4f29e
Zareel Kalam <zareelbwd@gmail.com>
2024-01-05 20:06:12 +0530

    completed more tasks

---
+++ 17inheritance.js
@@ -0,0 +1,28 @@
+//* Use Inheritance So You Don't Repeat Yourself
+
+//? The eat method is repeated in both Cat and Bear. Edit the code in the spirit of DRY by moving the eat method to the Animal supertype.
+
+function Cat(name) {
+  this.name = name;
+}
+
+Cat.prototype = {
+  constructor: Cat,
+};
+
+function Bear(name) {
+  this.name = name;
+}
+
+Bear.prototype = {
+  constructor: Bear,
+};
+
+function Animal() {}
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function () {
+    console.log("nom nom nom");
+  },
+};
